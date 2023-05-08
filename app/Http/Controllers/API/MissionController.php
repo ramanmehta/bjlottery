afcf68370a0d5ae696be2e5c43ca6b9988503f7c
ramanmehta <ramanrax@gmail.com>
2023-05-09 01:54:48 +0530

    code added for mission AP

--- app/Http/Controllers/API/MissionController.php
+++ app/Http/Controllers/API/MissionController.php
@@ -5,24 +5,46 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mission;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\MissionSubmission;
 class MissionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        // with(['missionLevels'])
-        if (auth('sanctum')->check()) {
-            $userId = auth('sanctum')->user()->id;
-            $mission = Mission::with(['missionSubmission' => function ($query,$userId) {
-                $query->where('user_id', $userId);
-            }])->where('status',1)->get();
+    public function index(Request $request)
+    {   
+        $user_id = $request->input('user_id');
+        if ($user_id) {
+            $mission = Mission::where('missions.status',1)->get();
             if($mission->count() > 0){
+                $missionData = [];
+                foreach($mission as $missions){
+                        $missionsCount = MissionSubmission::where('mission_id',$missions->id)->where('user_id',$user_id)->count();
+                        if($missionsCount > 0){
+                            $approvalStatus = MissionSubmission::where('mission_id',$missions->id)->where('user_id',$user_id)->first();
+                            $approval_status = $approvalStatus->approval_status;
+                        }else{
+                            $approval_status = "";
+                        }
+                        $missionData[] = ['id'=>$missions->id,
+                                            'mission_title'=>$missions->mission_title,
+                                            'mission_description'=>$missions->mission_description,
+                                            'mission_proof_type'=>$missions->mission_proof_type,
+                                            'number_of_share'=>$missions->number_of_share,
+                                            'referral_apoint'=>$missions->per_share_point,
+                                            'approval_status'=>$approval_status,
+                                            'created_at'=>$missions->created_at,
+                                            'updated_at'=>$missions->updated_at,
+                                            'banner_image'=>$missions->banner_image
+                                        ];
+                    
+                }
                 return response()->json([
                     'status' => 200,
-                    'mission' => $mission,
+                    'mission' => $missionData,
                 ], 200);
             }else{
                 return response()->json([
@@ -35,7 +57,7 @@ class MissionController extends Controller
                 'status' => 404,
                 'message' => 'No record found',
             ], 404);
-        }        
+        }
     }
 
     
