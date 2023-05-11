182351019a6cd3b17214b6874eab4e7642bea9d8
Haresh Chauhan <hareshc1999@gmail.com>
2023-05-11 16:50:20 +0530

    update

--- app/Http/Controllers/API/MissionController.php
+++ app/Http/Controllers/API/MissionController.php
@@ -5,54 +5,57 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mission;
+use App\Models\MissionLevel;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use App\Models\MissionSubmission;
+use Illuminate\Auth\Events\Validated;
+
 class MissionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
-    {   
+    {
         $user_id = $request->input('user_id');
         if ($user_id) {
-            $mission = Mission::where('missions.status',1)->get();
-            if($mission->count() > 0){
+            $mission = Mission::where('missions.status', 1)->get();
+            if ($mission->count() > 0) {
                 $missionData = [];
-                foreach($mission as $missions){
-                        $missionsCount = MissionSubmission::where('mission_id',$missions->id)->where('user_id',$user_id)->count();
-                        if($missionsCount > 0){
-                            $approvalStatus = MissionSubmission::where('mission_id',$missions->id)->where('user_id',$user_id)->first();
-                            $approval_status = $approvalStatus->approval_status;
-                        }else{
-                            $approval_status = "";
-                        }
-                        $missionData[] = ['id'=>$missions->id,
-                                            'mission_title'=>$missions->mission_title,
-                                            'mission_description'=>$missions->mission_description,
-                                            'mission_proof_type'=>$missions->mission_proof_type,
-                                            'number_of_share'=>$missions->number_of_share,
-                                            'referral_apoint'=>$missions->per_share_point,
-                                            'approval_status'=>$approval_status,
-                                            'created_at'=>$missions->created_at,
-                                            'updated_at'=>$missions->updated_at,
-                                            'banner_image'=>$missions->banner_image
-                                        ];
-                    
+                foreach ($mission as $missions) {
+                    $missionsCount = MissionSubmission::where('mission_id', $missions->id)->where('user_id', $user_id)->count();
+                    if ($missionsCount > 0) {
+                        $approvalStatus = MissionSubmission::where('mission_id', $missions->id)->where('user_id', $user_id)->first();
+                        $approval_status = $approvalStatus->approval_status;
+                    } else {
+                        $approval_status = "";
+                    }
+                    $missionData[] = [
+                        'id' => $missions->id,
+                        'mission_title' => $missions->mission_title,
+                        'mission_description' => $missions->mission_description,
+                        'mission_proof_type' => $missions->mission_proof_type,
+                        'number_of_share' => $missions->number_of_share,
+                        'referral_apoint' => $missions->per_share_point,
+                        'approval_status' => $approval_status,
+                        'created_at' => $missions->created_at,
+                        'updated_at' => $missions->updated_at,
+                        'banner_image' => $missions->banner_image
+                    ];
                 }
                 return response()->json([
                     'status' => 200,
                     'mission' => $missionData,
                 ], 200);
-            }else{
+            } else {
                 return response()->json([
                     'status' => 404,
                     'message' => 'No record found',
                 ], 404);
             }
-        }else{
+        } else {
             return response()->json([
                 'status' => 404,
                 'message' => 'No record found',
@@ -66,12 +69,12 @@ class MissionController extends Controller
     public function show(string $id)
     {
         $mission = Mission::find($id);
-        if($mission){
+        if ($mission) {
             return response()->json([
                 'status' => 200,
                 'Mission' => $mission
             ], 200);
-        }else{
+        } else {
             return response()->json([
                 'status' => 404,
                 'Message' => 'No record found',
@@ -103,27 +106,67 @@ class MissionController extends Controller
         //
     }
 
-    public function list(Request $request,$id=null)
+    public function list(Request $request, $id = null)
     {
-        $mission = Mission::where('status',1)
-            ->select('id',
+        $mission = Mission::where('status', 1)
+            ->select(
+                'id',
                 'mission_title',
                 'mission_description',
                 'banner_image',
                 'mission_type',
                 'enter_earn_affliated_points',
                 'prize_name',
-                'prize_image')
-            ->when(! is_null($id),function($q) use ($id){
-                $q->where('id',$id);
+                'prize_image'
+            )
+            ->when(!is_null($id), function ($q) use ($id) {
+                $q->where('id', $id);
             })
-            ->orderBy('id','desc')
+            ->orderBy('id', 'desc')
             ->get();
 
         return response()->json([
             'status' => 200,
             'Message' => 'Mission list',
             'data' => $mission
+        ], 200);
+    }
+
+    public function missionSubmit(Request $request)
+    {
+        $validated = \Validator::make($request->all(), [
+            'image' => 'required_without:video|image|mimes:jpeg,jpg,gif,png',
+            'video' => 'required_without:image|mimes:mp4,mov,ogg,qt|max:10240',
+            'mission_id' => 'required'
+        ]);
+
+        $input = $validated->validate();
+
+        if (isset($input['image'])) {
+
+            $data['proof'] = 'missions/' . time() . '.' . $request->image->extension();
+
+            $request->image->storeAs('public/images', $data['proof']);
+        } else {
+
+            $fileName = $request->video->getClientOriginalName();
+
+            $data['proof'] = 'missions/' . $fileName;
+
+            $request->video->storeAs('public/images', $data['proof']);
+        }
+
+        MissionSubmission::create([
+            'mission_id' => $input['mission_id'],
+            'proof' => $data['proof'],
+            'user_id' => auth()->id(),
+            'approval_status' => 'submit'
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'Message' => 'Mission Submited successfully',
+            'data' => []
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\RoleController;
@@ -27,17 +28,18 @@ use App\Http\Controllers\API\LuckeyWinnerController;
 //     return $request->user();
 // });
 
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('auth:sanctum')->group(function () {
 
-    Route::post('/user',[AuthController::class, 'user']);
-    Route::post('/logout',[AuthController::class, 'logout']);
-    Route::post('/passwordChange',[AuthController::class, 'passwordChange']);
+    Route::post('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/passwordChange', [AuthController::class, 'passwordChange']);
     //Route::post('/profile-update',[AuthController::class, 'updateUser']);
-    Route::post('/profile-update',[AuthController::class, 'profileUpdate']);
-    Route::get('lotter-prize/{id?}',[LuckeyWinnerController::class,'lotterPrizeWinner']);
-    Route::post('user-lotter-prize/claim',[LuckeyWinnerController::class,'lotterPrizeClaim']);
+    Route::post('/profile-update', [AuthController::class, 'profileUpdate']);
+    Route::get('lotter-prize/{id?}', [LuckeyWinnerController::class, 'lotterPrizeWinner']);
+    Route::post('user-lotter-prize/claim', [LuckeyWinnerController::class, 'lotterPrizeClaim']);
 
-    Route::get('mission/{id?}',[MissionController::class,'list']);
+    Route::get('mission/{id?}', [MissionController::class, 'list']);
+    Route::post('mission/submit', [MissionController::class, 'missionSubmit']);
 });
 
 // Route::middleware('auth:sanctum')->post('/user', [AuthController::class, 'user']);
@@ -48,77 +50,76 @@ Route::middleware('auth:sanctum')->group(function(){
 
 // Route::middleware('auth:sanctum')->post('/profile-update', [AuthController::class, 'updateUser']);
 
-Route::controller(AuthController::class)->group(function(){
-    Route::post('login','login');
-    Route::post('register','register');
-    Route::post('referal-register/{ref}','register');
-    Route::post('forgetPassword','forgetPassword');
-    Route::post('changePassword','changePassword');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('referal-register/{ref}', 'register');
+    Route::post('forgetPassword', 'forgetPassword');
+    Route::post('changePassword', 'changePassword');
     Route::get('userwalletap', 'userwalletap');
     Route::get('redeemPoints', 'redeemPoints');
-    
 });
 
-Route::controller(RoleController::class)->group(function(){
-    
-    Route::get('roles','index');
-    Route::get('role/{id}','show');
+Route::controller(RoleController::class)->group(function () {
+
+    Route::get('roles', 'index');
+    Route::get('role/{id}', 'show');
 });
 
-Route::controller(DailyRewardController::class)->group(function(){
-    Route::get('dailyreward' , 'index');
-    Route::get('dailyreward/{id}' , 'show');
-    Route::post('' , '');
-    Route::post('' , '');
+Route::controller(DailyRewardController::class)->group(function () {
+    Route::get('dailyreward', 'index');
+    Route::get('dailyreward/{id}', 'show');
+    Route::post('', '');
+    Route::post('', '');
 });
 
-Route::controller(RewardTypeController::class)->group(function(){
-    Route::get('rewardtype' , 'index');
-    Route::get('getrewardtype/{id}' , 'show');
-    Route::post('' , '');
-    Route::post('' , '');
+Route::controller(RewardTypeController::class)->group(function () {
+    Route::get('rewardtype', 'index');
+    Route::get('getrewardtype/{id}', 'show');
+    Route::post('', '');
+    Route::post('', '');
 
     // get reward point
-    ROute::post('dailyRewardPoints' , 'dailyRewardPoints'); 
-    ROute::post('weeklyRewardPoints' , 'weeklyRewardPoints'); 
+    ROute::post('dailyRewardPoints', 'dailyRewardPoints');
+    ROute::post('weeklyRewardPoints', 'weeklyRewardPoints');
 });
 
 // Route::post('dailyRewardPoints',[DailyRewardController::class,'dailyRewardPoints']);
 // Route::post('weeklyRewardPoints',[DailyRewardController::class,'weeklyRewardPoints']);
 
-Route::controller(LuckyDrawGamesController::class)->group(function(){
-    Route::get('allluckydraws' , 'index');
-    Route::get('luckydraw/{id}' , 'show');
-    Route::post('' , '');
+Route::controller(LuckyDrawGamesController::class)->group(function () {
+    Route::get('allluckydraws', 'index');
+    Route::get('luckydraw/{id}', 'show');
+    Route::post('', '');
     // Route::post('getNumber' , 'getNumber');
     // Route::get('totalParticipant' , 'totalParticipant');
     // Route::post('userNumber' , 'userNumber');
 
-    Route::post('getNumber' , 'getNumber');
-    Route::get('totalParticipant' , 'totalParticipant');
-    Route::get('participantUsername' , 'participantUsername');
-    Route::post('userNumber' , 'userNumber');
+    Route::post('getNumber', 'getNumber');
+    Route::get('totalParticipant', 'totalParticipant');
+    Route::get('participantUsername', 'participantUsername');
+    Route::post('userNumber', 'userNumber');
 });
 
-Route::controller(MissionController::class)->group(function(){
-    Route::get('mission' , 'index');
-    Route::get('mission/{id}' , 'show');
-    Route::get('' , '');
-    Route::post('' , '');
-    Route::post('userMission/{id}' , 'userMission');
+Route::controller(MissionController::class)->group(function () {
+    Route::get('mission', 'index');
+    Route::get('mission/{id}', 'show');
+    Route::get('', '');
+    Route::post('', '');
+    Route::post('userMission/{id}', 'userMission');
 });
 
-Route::controller(NotificationController::class)->group(function(){
-    Route::get('notification' , 'index');
-    Route::get('notification/{id}' , 'show');
-    Route::post('' , '');
-    Route::get('' , '');
+Route::controller(NotificationController::class)->group(function () {
+    Route::get('notification', 'index');
+    Route::get('notification/{id}', 'show');
+    Route::post('', '');
+    Route::get('', '');
 });
 
-Route::controller(ReferalsStatsController::class)->group(function(){
-    Route::get('referalsStats' , 'index');
-    Route::get('referalsStats/{id}' , 'show');
-    Route::post('getYourHIstory' , 'getYourHIstory');
-    Route::post('' , '');
-    Route::post('' , '');
+Route::controller(ReferalsStatsController::class)->group(function () {
+    Route::get('referalsStats', 'index');
+    Route::get('referalsStats/{id}', 'show');
+    Route::post('getYourHIstory', 'getYourHIstory');
+    Route::post('', '');
+    Route::post('', '');
 });
